b1a2366aac557c8af4b5c9de07499a5b1014f55d
lincentpega <imkrasnyukov@gmail.com>
2024-04-28 22:59:46 +0500

    initialize project

---
+++ src/main/java/com/lincentpega/localp2p/LocalP2pApplication.java
@@ -0,0 +1,13 @@
+package com.lincentpega.localp2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalP2pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalP2pApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lincentpega/localp2p/LocalP2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincentpega.localp2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalP2pApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
